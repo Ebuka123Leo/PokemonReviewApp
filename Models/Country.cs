c152cf29750f7fb12db105ca5ceee7baa10b94ab
HP <HP@EBUKA>
2023-09-14 09:55:59 +0100

    Add project files.

---
+++ Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Owner> Owners{ get; set; }
+    }
+}
---
+++ Models/Owner.cs
@@ -0,0 +1,17 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gym { get; set; }
+
+        public Country Country { get; set; }
+
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+    }
+}
